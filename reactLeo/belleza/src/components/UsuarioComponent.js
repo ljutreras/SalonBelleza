86a4fc086c593cd29377e7dcc8b5746233b26d94
ljutreras <ljutreras96@gmail.com>
2022-07-14 21:50:06 -0400

    First Commit

---
+++ reactLeo/belleza/src/components/UsuarioComponent.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "../styles/Nav.css";
+
+const UsuarioComponent=()=>{
+    return(
+        <div>
+            <header>
+                <div id="logo-header">
+
+                </div>
+            </header>
+                <div id="nav-bar">
+
+                </div>
+            <aside>
+                <div id="carousel-nav">
+
+                </div>
+                
+            </aside>
+            <footer>
+                <div class="container-footer">
+                    <div id="horario-footer">
+
+                    </div>
+                    <div id="service-footer">
+
+                    </div>
+                    <div id="contact-footer">
+
+                    </div>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default UsuarioComponent;
