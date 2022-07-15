80b8e008dda845946cce24e5122eae323119fb79
ljutreras <ljutreras96@gmail.com>
2022-07-15 00:18:22 -0400

    Creacion de la maqueta

--- reactLeo/belleza/src/components/UsuarioComponent.js
+++ reactLeo/belleza/src/components/UsuarioComponent.js
@@ -1,33 +1,94 @@
 import React from "react";
 import "../styles/Nav.css";
 
-const UsuarioComponent=()=>{
-    return(
+const UsuarioComponent = () => {
+    return (
         <div>
             <header>
                 <div id="logo-header">
 
                 </div>
             </header>
-                <div id="nav-bar">
+            <div id="nav-bar">
 
-                </div>
+            </div>
             <aside>
                 <div id="carousel-nav">
 
                 </div>
-                
+
             </aside>
             <footer>
                 <div class="container-footer">
                     <div id="horario-footer">
+                        <article>
+                            <header>
+                                Horario de atención
+                            </header>
+                            <br />
+                            <h3>
+                                De lunes a viernes
+                            </h3>
+                            <p>
+                                de 9.30 a 13.30
+                                de 15.30 a 20.00
+                            </p>
+                            <br />
+
+                            <h3>
+                                Sábados
+                            </h3>
+                            <p>
+                                de 9.30 a 14.30
+                            </p>
+                        </article>
 
                     </div>
                     <div id="service-footer">
+                    <article>
+                            <header>
+                                Horario de atención
+                            </header>
+                            <br />
+                            <h3>
+                                De lunes a viernes
+                            </h3>
+                            <p>
+                                de 9.30 a 13.30
+                                de 15.30 a 20.00
+                            </p>
+                            <br />
 
+                            <h3>
+                                Sábados
+                            </h3>
+                            <p>
+                                de 9.30 a 14.30
+                            </p>
+                        </article>
                     </div>
                     <div id="contact-footer">
+                    <article>
+                            <header>
+                                Horario de atención
+                            </header>
+                            <br />
+                            <h3>
+                                De lunes a viernes
+                            </h3>
+                            <p>
+                                de 9.30 a 13.30
+                                de 15.30 a 20.00
+                            </p>
+                            <br />
 
+                            <h3>
+                                Sábados
+                            </h3>
+                            <p>
+                                de 9.30 a 14.30
+                            </p>
+                        </article>
                     </div>
                 </div>
             </footer>
